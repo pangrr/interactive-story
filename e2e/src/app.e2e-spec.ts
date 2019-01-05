dced8030eb1b02658656618448ddf7b6e49a448a
Ran Pang <ran@ChendeMBP.home>
2019-01-05 11:23:10 -0500

    rename

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to play-story!');
+    expect(page.getTitleText()).toEqual('Welcome to playstory!');
   });
 });
